0dd262b246b7e087b71620cf66a2b7d9803a7c71
kaitodecode <kaitodecode@gmail.com>
2025-08-15 14:59:13 +0700

    refactor: update package name and add firebase dependencies
    - Rename package from com.kaitodecode.utcareers to com.mawar.ujikom
    - Add firebase and expo-notifications dependencies
    - Configure EAS build settings

--- android/app/src/main/java/com/mawar/ujikom/MainActivity.kt
+++ android/app/src/main/java/com/mawar/ujikom/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.kaitodecode.utcareers
+package com.mawar.ujikom
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/mawar/ujikom/MainApplication.kt
+++ android/app/src/main/java/com/mawar/ujikom/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.kaitodecode.utcareers
+package com.mawar.ujikom
 
 import android.app.Application
 import android.content.res.Configuration
